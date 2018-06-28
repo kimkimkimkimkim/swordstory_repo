56a2b6df26d7ca4af9253526362fb0eb1129f4c7
SaneYuki-m <s.makino@fuji.waseda.jp>
2018-06-28 12:49:54 +0900

    morning628
    morning628

---
+++ SwordStory/Assets/Script/BattleScene/OrbManeger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbManeger : MonoBehaviour {
+
+	private GameObject gameManager;
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.Find("OrbManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TouchOrb(){
+		if(Input.GetMouseButton(0) == false){
+			return;
+		}
+		Debug.Log("orbtouched");
+		Destroy(this.gameObject);
+	}
+}
